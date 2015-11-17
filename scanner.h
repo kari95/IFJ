293eb79aa2373048a218f35104aaa59be0300525
Miroslav Karasek <xkaras31@stud.fit.vutbr.cz>
2015-11-18 00:26:00 +0100

    added token types

--- scanner.h
+++ scanner.h
@@ -16,11 +16,28 @@
 
 // types of token
 typedef enum {
-    ID_TO,
-    INT_TO,
-    DOUBLE_TO,
-    STRING_TO,
-
+    ID_TO,          // identificator
+    INT_TO,         // xxxxx
+    DOUBLE_TO,      // x.xxx
+    STRING_TO,      // ""
+    ASSIGN_TO,      // =
+    LBRACKET_TO,    // (
+    RBRACKET_TO,    // )  
+    SEMI_TO,        // ;
+    COMMA_TO,       // ,
+    LCBRACKET_TO,   // {
+    RCBRACKET_TO,   // }  
+    OUT_TO,         // <<
+    IN_TO,          // >>
+    PLUS_TO,        // +
+    MINUS_TO,       // -
+    MUL_TO,         // *
+    DIV_TO,         // /
+    G_TO,           // >
+    L_TO,           // <
+    GE_TO,          // >=
+    LE_TO,          // <=
+    EOF_TO          // end of file
 } tokenType_T;
 
 // struct to save token
@@ -29,7 +46,8 @@ typedef struct {
 	void *data;
 } token_T;
 
-// read token from 'programFile'
-token_T getToken(FILE *programFile);
+// read token from 'programFile' to structure 'token'
+// return zero if success
+int getToken(FILE *programFile, token_T *token);
 
 #endif 
